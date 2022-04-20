35d91f65f1cd3d8af3edacd359152356755a4bd2
Jelani Alexis <jelanialexis5@gmail.com>
2022-04-20 11:54:50 +0200

    hotfix (again bc I can't test properly): n-word counter now ACTUALLY counts capital letters

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -79,7 +79,7 @@ module.exports = new ClientEvent("messageCreate", async (message) => {
 
 
     // Adds 1 to the N-word counter
-    if (message.content.includes('nigga')){
+    if (message.content.toLowerCase().includes('nigga')){
         const count = (message.content.toLowerCase().match(/nigga/g) || []).length;
         await profileModel.findOneAndUpdate({ userID: message.author.id },
             {
@@ -87,7 +87,7 @@ module.exports = new ClientEvent("messageCreate", async (message) => {
                     softRs: count,
                 }
         });
-    } else if (message.content.includes('nigger')){
+    } else if (message.content.toLowerCase().includes('nigger')){
         const count = (message.content.toLowerCase().match(/nigger/g) || []).length;
         await profileModel.findOneAndUpdate({ userID: message.author.id },
             {
