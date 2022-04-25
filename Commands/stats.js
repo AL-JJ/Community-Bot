8b34b9ea3022f87422c827d1551dccfdde07b0a4
Jelani Alexis <jelanialexis5@gmail.com>
2022-04-25 14:30:23 +0200

    !!stats now able to show other people's stats

--- Commands/stats.js
+++ Commands/stats.js
@@ -1,4 +1,4 @@
-const Command = require('../BaseClasses/Command');
+Command = require('../BaseClasses/Command');
 const profileModel = require("../Database/Models/profileSchema");
 
 module.exports = new Command(
@@ -8,18 +8,26 @@ module.exports = new Command(
     "Displays the user's stats.",
     async (message, args) => {
         let profile;
-        profile = await profileModel.findOne({ userID: message.author.id });
+
+        if (args.length === 1) {
+            if (!message.mentions.users.first()) return message.reply("Please mention a user.");
+            console.log('more than 1');
+            profile = await profileModel.findOne({ userID: message.mentions.users.first().id });
+        }
+        else profile = await profileModel.findOne({ userID: message.author.id });
         console.log(profile);
 
         const embed = {
             color: 0x2F3136,
-            title: `Stats for ${message.author.tag}`,
             fields: [
                 { name: "Level", value: `${profile.level}` },
                 { name: "Current XP", value: `${profile.xp} (${((Math.ceil(4.5 * profile.level) + 80) + 3 * (profile.level ** 3) - profile.xp)} left for next level)` },
                 { name: "N-words", value: `${profile.softRs + profile.hardRs}, of which ${profile.hardRs} were hard-r's.`}
             ]
         }
+
+        if (args.length === 1) embed.title = `Stats for ${ message.mentions.users.first().username }`;
+        else embed.title = `Stats for ${ message.author.username }`;
 
         message.reply({ embeds: [embed] });
     }
